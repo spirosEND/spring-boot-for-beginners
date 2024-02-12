f81a5d6eea2257ec7ad35320794a9dffc56331e6
Spiros Vatikiotis <spirosfwiw@gmail.com>
2024-02-12 02:01:33 +0200

    implementing service layer

--- src/main/java/com/spiros/springdemo/student/StudentController.java
+++ src/main/java/com/spiros/springdemo/student/StudentController.java
@@ -4,45 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentController {
 
+
+    private StudentService service;
+
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
     @GetMapping
     public List<Student> findAllStudents() {
-        return List.of(
-                new Student(
-                        "Spiros",
-                        "Vatikiotis",
-                        LocalDate.now(),
-                        "spirosfwiw@gmail.com",
-                        41),
-                new Student(
-                        "Vaggos",
-                        "Mixalakeas",
-                        LocalDate.now(),
-                        "psionicv@hotmail.com",
-                        42
-                ),
-
-                new Student(
-                        "George",
-                        "Antipatis",
-                        LocalDate.now(),
-                        "georgeantipatis@hotmail.com",
-                        41
-                ),
-
-                new Student(
-                        "Mike",
-                        "Mixalakeas",
-                        LocalDate.now(),
-                        "mikemixalakeas@hotmail.com",
-                        39
-                )
-        );
+        return service.findAllStudents();
     }
 }
--- src/main/java/com/spiros/springdemo/student/StudentService.java
+++ src/main/java/com/spiros/springdemo/student/StudentService.java
@@ -1,4 +1,51 @@
 package com.spiros.springdemo.student;
 
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
 public class StudentService {
+
+    public List<Student> findAllStudents() {
+        return List.of(
+                new Student(
+                        "Spiros",
+                        "Vatikiotis",
+                        LocalDate.now(),
+                        "spirosfwiw@gmail.com",
+                        41),
+                new Student(
+                        "Vaggos",
+                        "Mixalakeas",
+                        LocalDate.now(),
+                        "psionicv@hotmail.com",
+                        42
+                ),
+
+                new Student(
+                        "George",
+                        "Antipatis",
+                        LocalDate.now(),
+                        "georgeantipatis@hotmail.com",
+                        41
+                ),
+
+                new Student(
+                        "Mike",
+                        "Mixalakeas",
+                        LocalDate.now(),
+                        "mikemixalakeas@hotmail.com",
+                        39
+                ),
+                new Student(
+                        "John",
+                        "Vodinos",
+                        LocalDate.now(),
+                        "johnvodinos@hotmail.com",
+                        45
+                )
+        );
+    }
 }
